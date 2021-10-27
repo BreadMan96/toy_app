10d9e4323f126530f8fc2cb8094f577920889f4a
BreadMan96 <vlad962706@mail.ru>
2021-10-27 14:07:18 +0300

    Add db/test in gitignore, add test title, refactoring home/about/help page, add Contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,32 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+   @base_title = "BreadMan training site"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | BreadMan training site"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | BreadMan training site"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | BreadMan training site"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | BreadMan training site"
   end
 end
